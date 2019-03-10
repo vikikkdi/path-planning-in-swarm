07d7ab48269162edb81da8f7d1a4b726385d5d34
Vikraman <vikikkdi@gmail.com>
2019-03-10 14:31:50 +0530

    correct move_ball code

--- path.py
+++ path.py
@@ -1,57 +1,80 @@
+import math
 import numpy as np
 from hungarian import Hungarian
 from tkinter import *
+from vis import Ball
 
-n = int(input("Enter the number of UAVs"))
-#n = 5
+#n = int(input("Enter the number of UAVs"))
+n = 9
 initial_pos = []
-for i in range(n):
-	x = int((np.random.rand()*10**5)%600)
-	y = int((np.random.rand()*10**5)%600)
-	initial_pos.append([x, y])
+x = 100
+y = 10
+initial_pos.append([x,y])
+x = 100
+y = 50
+initial_pos.append([x,y])
+x = 100
+y = 100
+initial_pos.append([x,y])
+x = 150
+y = 10
+initial_pos.append([x,y])
+x = 150
+y = 50
+initial_pos.append([x,y])
+x = 150
+y = 100
+initial_pos.append([x,y])
+x = 200
+y = 10
+initial_pos.append([x,y])
+x = 200
+y = 50
+initial_pos.append([x,y])
+x = 200
+y = 100
+initial_pos.append([x,y])
 
 desired_shape = []
-desired_shape.append([int((np.random.rand()*10**5)%600), int((np.random.rand()*10**5)%600)])
-for i in range(n-1):
-	desired_shape.append([int((desired_shape[-1][0]+10)%600), int((desired_shape[-1][1]+10)%600)])
+x = 100
+y = 200
+desired_shape.append([x,y])
+x = 150
+y = 250
+desired_shape.append([x,y])
+x = 200
+y = 300
+desired_shape.append([x,y])
+x = 250
+y = 350
+desired_shape.append([x,y])
+x = 300
+y = 400
+desired_shape.append([x,y])
+x = 350
+y = 350
+desired_shape.append([x,y])
+x = 400
+y = 300
+desired_shape.append([x,y])
+x = 450
+y = 250
+desired_shape.append([x,y])
+x = 500
+y = 200
+desired_shape.append([x,y])
 
 k = [[0 for j in range(n)] for i in range(n)]
 
 for i in range(n):
 	for j in range(n):
-		x = -np.transpose(initial_pos[i])*np.array(desired_shape[j])
-
-		val = x[0]
-		for _ in range(len(x)-1):
-			val += x[_+1]
-
-		k[i][j] = val
-#		break
-#	break
-
-print(k)
-
+		x = -np.transpose(initial_pos[i]).dot(np.array(desired_shape[j]))
+		k[i][j] = x
 
 hungarian = Hungarian(k)
 hungarian.calculate()
 x_star = hungarian.get_results()
 k_star = hungarian.get_total_potential()
-
-
-class Ball:
-    def __init__(self, canvas, x1, y1, fill_col):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x1+5
-        self.y2 = y1+5
-        self.canvas = canvas
-        self.ball = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill=fill_col)
-
-    def move_ball(self):
-        deltax = 1
-        deltay = 1
-        self.canvas.move(self.ball, deltax, deltay)
-        self.canvas.after(50, self.move_ball)
 
 # initialize root Window and canvas
 root = Tk()
@@ -63,11 +86,26 @@ canvas.pack()
 # create two ball objects and animate them
 balls = []
 color = ["red", "green", "black", "orange", "blue", "yellow", "purple", "grey", "brown", "magenta"]
+
+ds = 0
+for i in desired_shape:
+	x = np.transpose(i).dot(np.array(i))
+	ds += x
+alpha_star = (np.transpose(initial_pos).dot(np.array(desired_shape))+n*k_star)/(np.transpose(desired_shape).dot(np.array(desired_shape))-(n*ds))
+
+d_star = (initial_pos - np.transpose(alpha_star.dot(np.transpose(desired_shape))))/n
+
+q = np.transpose(alpha_star.dot(np.transpose(desired_shape)) + np.transpose(d_star))
+q = q%600
+
+print(q)
 _ = 0
 for i, j in x_star:
 	ball1 = Ball(canvas, initial_pos[i][0], initial_pos[i][1], color[_])
 	ball2 = Ball(canvas, desired_shape[j][0], desired_shape[j][1], color[_])
-	balls.append([ball1, ball2])
+	#ball2 = Ball(canvas, q[j][0], q[j][1], color[0])
+	ball1.move_ball(desired_shape[j][0], desired_shape[j][1])
 	_ = int(_ + 1)%10
+
 
 root.mainloop()
---
+++ vis.py
@@ -0,0 +1,67 @@
+from tkinter import *
+from random import randint
+
+class Ball:
+	def __init__(self, canvas, x1, y1, col):
+		self.x1 = x1 - 5
+		self.y1 = y1 - 5
+		self.x2 = x1 + 5
+		self.y2 = y1 + 5
+		self.x = x1
+		self.y = y1
+		self.canvas = canvas
+		self.ball = canvas.create_oval(self.x1, self.y1, self.x2, self.y2, fill=col)
+
+	def move_ball(self, x_aug, y_aug):
+		if self.x == x_aug and self.y == y_aug:
+			deltax = 0
+			deltay = 0
+		elif self.x == x_aug:
+			deltax = 0
+			if self.y < y_aug:
+				deltay = min(1, y_aug - self.y)
+			else:
+				deltay = max(-1, y_aug - self.y)
+		elif self.y == y_aug:
+			deltay = 0
+			if self.x < x_aug:
+				deltax = min(1, x_aug - self.x)
+			else:
+				deltax = max(-1, x_aug - self.x)
+		else:
+			x1 = self.x
+			x2 = x_aug
+			y1 = self.y
+			y2 = y_aug
+
+			c = (x2*y1 - x1*y2)/(x2-x1)
+			m = (y1-c)/x1
+
+			if x2 > x1:
+				deltax = min(1, x2 - x1)
+			else:
+				deltax = max(-1, x2 - x1)
+			x = x1 + deltax
+			y = m*x + c
+			deltay = y - y1
+		self.x = self.x + deltax
+		self.y = self.y + deltay
+		self.canvas.move(self.ball, deltax, deltay)
+		self.canvas.after(50, self.move_ball, x_aug, y_aug)
+
+if __name__ == '__main__':
+	# initialize root Window and canvas
+	root = Tk()
+	root.title("Balls")
+	root.resizable(True,True)
+	canvas = Canvas(root, width = 600, height = 600)
+	canvas.pack()
+
+	# create two ball objects and animate them
+	ball1 = Ball(canvas, 300, 300)
+	ball2 = Ball(canvas, 100, 100)
+
+	ball1.move_ball(ball2.x, ball2.y)
+	#ball2.move_ball()
+
+	root.mainloop()
