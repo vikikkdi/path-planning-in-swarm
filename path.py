correct gradient descent code with my cost function

path.py
@@ -7,10 +7,9 @@ import gradient_descent
 from time import sleep
 import coordinates
 
-#n = int(input("Enter the number of UAVs"))
 n = 9
 initial_pos = coordinates.initial_pos
-desired_shape = coordinates.generate_circle(n)
+desired_shape = coordinates.generate_V(n)
 
 k = [[0 for j in range(n)] for i in range(n)]
 
@@ -35,22 +34,14 @@ canvas.pack()
 balls = []
 color = ["red", "green", "black", "orange", "blue", "yellow", "purple", "grey", "brown", "magenta"]
 
-balls = []
+d = gradient_descent.gradient_descent(initial_pos, desired_shape, x_star)
+qq = np.array(d) + np.array(desired_shape)
 
-qq = []
-dist = 100000
-for x in range(-100, 100):
-	for y in range(-100, 100):
-		q = np.array([x,y])+np.array(desired_shape)
-		dist_1 = gradient_descent.calculate_distance(initial_pos, q, x_star)
-		if dist > dist_1:
-			dist = dist_1
-			qq = q 
 _ = 0
 for i, j in x_star:
 	ball1 = Ball(canvas, initial_pos[i][0], initial_pos[i][1], color[_])
-#	ball2 = Ball(canvas, qq[j][0], qq[j][1], color[_])
-#	ball2 = Ball(canvas, desired_shape[j][0], desired_shape[j][1], color[0])
+	ball2 = Ball(canvas, qq[j][0], qq[j][1], color[_])
+#	ball2 = Ball(canvas, desired_shape[j][0], desired_shape[j][1], color[2])
 	ball1.move_ball(qq[j][0], qq[j][1])
 	_ = int(_ + 1)%10
 